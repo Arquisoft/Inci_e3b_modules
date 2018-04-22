08c5ade3cbe32270dbaca3087abd354c51708de1
Kilian Pérez González <kilianpg@gmail.com>
2018-04-23 01:57:42 +0200

    Agrega ejecutor de la pruebas de aceptación de usuario con Cucumber
    	mvn -Dit.test=AcceptanceTests verify
    
    Ver también: Issue #11

---
+++ InciTest_e3b/src/test/java/es/uniovi/asw/e3b/incitest_e3b/acceptancetests/AcceptanceTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.asw.e3b.incitest_e3b.acceptancetests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = {"es.uniovi.asw.e3b.incitest_e3b.acceptancetests.step" }, 
+		features = "src/test/resources/features", 
+		plugin = {"pretty", "html:target/cucumber", "default_summary"})
+public class AcceptanceTests {
+}
