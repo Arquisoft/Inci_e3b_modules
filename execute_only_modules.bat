6409bfd9671279c18a7a83392d89871555ca5511
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-06-08 14:18:17 +0200

    scripts windows #23 para ejecutar servicios y modulos de manera separada

---
+++ execute_only_modules.bat
@@ -0,0 +1,4 @@
+@echo off
+start cmd /k "title Agents_e3b & cd Agents_e3b & mvn spring-boot:run"
+start cmd /k "title InciManager_e3b & cd InciManager_e3b & mvn spring-boot:run"
+start cmd /k "title InciDashboard_e3b & cd InciDashboard_e3b & mvn spring-boot:run"
